497c43cacae402049759f5d0a3c5718f02f0a11c
grzban <grzegorz.banaszek@gmail.com>
2018-01-17 14:05:39 +0100

    v2.0

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*- 
+import os
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,19 +28,20 @@ def inputVeryfication(player, sign, board):
             p=int(p)
             if p in playButtons:
                 if board[str(p)] == 'X' or board[str(p)] == 'O':
-                    print("To pole jest już zajęte. Ponów próbę!")
+                    print("This field is occupied. Try again!")
                     continue
                 else:
                     break
             else:
-                print("Należy podać cyfrę od 1 - 9. Ponów próbę!")
+                print("Invalid field number. Enter a number between 1 and 9.")
                 continue
         except:
-            print("Wpisałeś dziwny znak! Należy podać cyfrę od 1 - 9. Ponów próbę!")
+            print("Invalid entry, try again!")
             continue
     return p
 
 def graphics(p):
+    os.system('clear')
     print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
     print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ p["7"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["8"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["9"]+ bcolors.ENDC,)
     print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
@@ -52,116 +54,106 @@ def graphics(p):
     print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ p["1"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["2"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["3"]+ bcolors.ENDC,)
     print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
 
-def checkx(p,P1,P2):
-    if p["1"]=="X" and p["2"]=="X" and p["3"]=="X":
-        print(bcolors.FAIL+P1," WIN!"+bcolors.ENDC)
-        exit(0)
-    elif p["4"]=="X" and p["5"]=="X" and p["6"]=="X":
-        print(P1," WIN!")
-        exit(0)
-    elif p["7"]=="X" and p["8"]=="X" and p["9"]=="X":
-        print(P1," WIN!")
-        exit(0)
-    elif p["1"]=="X" and p["4"]=="X" and p["7"]=="X":
-        print(P1," WIN!")
-        exit(0)
-    elif p["2"]=="X" and p["5"]=="X" and p["8"]=="X":
-        print(P1," WIN!")
-        exit(0)
-    elif p["3"]=="X" and p["6"]=="X" and p["9"]=="X":
-        print(P1," WIN!")
-        exit(0)
-    elif p["1"]=="X" and p["5"]=="X" and p["9"]=="X":
-        print(P1," WIN!")
-        exit(0)
-    elif p["3"]=="X" and p["5"]=="X" and p["7"]=="X":
-        print(P1," WIN!")
-        exit(0)
-
-def checko(p,P1,P2):
-    if p["1"]=="O" and p["2"]=="O" and p["3"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["4"]=="O" and p["5"]=="O" and p["6"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["7"]=="O" and p["8"]=="O" and p["9"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["1"]=="O" and p["4"]=="O" and p["7"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["2"]=="O" and p["5"]=="O" and p["8"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["3"]=="O" and p["6"]=="O" and p["9"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["1"]=="O" and p["5"]=="O" and p["9"]=="O":
-        print(P2," WIN!")
-        exit(0)
-    elif p["3"]=="O" and p["5"]=="O" and p["7"]=="O":
-        print(P2," WIN!")
-        exit(0)
+def check(p,P,sign):
+    if ((p["1"]==sign and p["2"]==sign and p["3"]==sign)
+        or(p["4"]==sign and p["5"]==sign and p["6"]==sign)
+        or(p["7"]==sign and p["8"]==sign and p["9"]==sign)
+        or(p["1"]==sign and p["4"]==sign and p["7"]==sign)
+        or(p["2"]==sign and p["5"]==sign and p["8"]==sign)
+        or(p["3"]==sign and p["6"]==sign and p["9"]==sign)
+        or(p["1"]==sign and p["5"]==sign and p["9"]==sign)
+        or(p["3"]==sign and p["5"]==sign and p["7"]==sign)):
+        print(bcolors.FAIL+P," WIN!"+bcolors.ENDC)
+        return False
+    return True
 
 def help():
     print("\n")
-    print(" Key input:")
+    print(bcolors.OKGREEN+" Key input:"+ bcolors.ENDC)
     print("\n")
-    print("        ||        ||")
-    print("    7   ||    8   ||   9")
-    print("        ||        ||")
-    print("==============================")
-    print("        ||        ||")
-    print("    4   ||    5   ||   6")
-    print("        ||        ||")
-    print("==============================")
-    print("        ||        ||")
-    print("    1   ||    2   ||   3")
-    print("        ||        ||")
+    print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
+    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ "7" + bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "8" + bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "9" + bcolors.ENDC,)
+    print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
+    print(bcolors.WARNING+ "==============================" + bcolors.ENDC)
+    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
+    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ "4"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "5"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "6" + bcolors.ENDC,)
+    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
+    print(bcolors.WARNING+ "=============================="+ bcolors.ENDC)
+    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
+    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ "1"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "2"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "3"+ bcolors.ENDC,)
+    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
     print("\n")
+
+def checkBoard(board):
+    if ' ' in board.values() :
+        return True
+    else:
+        print("REMIS")
+        return False
 
 def game(player1,player2):
     p=initGame()
-    flag=True
-    i=0
     graphics(p)
-    while flag:
+    flag=True
+    while True:
         p1 = inputVeryfication(player1,"X",p)
         setPosition("X",p1,p)
+        flag=checkBoard(p)
+        if not flag:
+            break
         graphics(p)
-        checkx(p,player1,player2)
-        checko(p,player1,player2)
+        flag = check(p,player1,"X")
+        if not flag:
+            break
+        flag = check(p,player2,"O")
+        if not flag:
+            break
         p2 = inputVeryfication(player2, "O",p)
         setPosition("O",p2,p)
+        flag=checkBoard(p)
+        if not flag:
+            break
         graphics(p)
-        checkx(p,player1,player2)
-        checko(p,player1,player2)
-        i+=1
-        if i==5:
+        flag=check(p,player1,"X")
+        if not flag:
+            break
+        flag = check(p,player2,"O")
+        if not flag:
             break
 
 def beginGame():
+    os.system('clear')
     print("\n")
     print(bcolors.OKGREEN+ " Welcome to Tic-Tac-Toe ver. 1.5!" + bcolors.ENDC)
 
     while True:
     #main menu
-        menu_select = input(bcolors.OKGREEN+ " Type \'s\' to start new game \n Type \'h\' for help \n :" + bcolors.ENDC)
+        menu_select = input(bcolors.OKGREEN+ " Type \'s\' to start new game \n Type \'h\' for help \n Type \'q\' to quit \n:" + bcolors.ENDC)
     #new game
         if menu_select == "s":
+            os.system('clear')
             P1=input(bcolors.OKGREEN+ "Enter name for Player One \n :" + bcolors.ENDC)
             P2=input(bcolors.OKGREEN+ "Enter name for Player Two \n :" + bcolors.ENDC)
             #game
             game(P1,P2)
         #help
         elif menu_select == "h":
+            os.system('clear')
             help_select = input(bcolors.OKGREEN+ "Type \'r\' for rules or \'i\' for key input \n :" + bcolors.ENDC)
             if help_select == "r":
+                os.system('clear')
                 print(bcolors.OKGREEN+ "Each player takes a turn and selects a field \n which they wish to place their marker on by \n typing a corresponding number. \n The goal of the game is to place 3 of your markers \n in a straight line (vertical, horizontal or diagonal)." + bcolors.ENDC)
             elif help_select == "i":
+                os.system('clear')
                 help()
-
+            else:
+                print(bcolors.FAIL+"Invalid entry, try again."+ bcolors.ENDC)
+                continue
+        elif menu_select == "q":
+            exit(0)
+        else:
+            print(bcolors.FAIL+"Invalid entry, try again."+ bcolors.ENDC)
+        continue
 
 def main():
     beginGame()
