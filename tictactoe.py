84128664822757fb02f7f47e6f436673f0b62fe6
grzban <grzegorz.banaszek@gmail.com>
2018-01-17 15:02:51 +0100

    Prepared to AI

--- tictactoe.py
+++ tictactoe.py
@@ -3,27 +3,24 @@
 import os
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    DEFAULT = '\033[0m'
 
 def title():
-    print(bcolors.FAIL+"xxxxxxxxxx   xx   xxxxxxxxxx      xxxxxxxxxx   xxxxxxxxxx   xxxxxxxxxx"+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx   xx                  xx       xx      xx   xx        "+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx   xx                  xx       xxxxxxxxxx   xx        "+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx   xx                  xx       xx      xx   xx        "+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx   xxxxxxxxxx          xx       xx      xx   xxxxxxxxxx"+ bcolors.ENDC)
+    print(bcolors.RED+"xxxxxxxxxx   xx   xxxxxxxxxx      xxxxxxxxxx   xxxxxxxxxx   xxxxxxxxxx"+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx   xx                  xx       xx      xx   xx        "+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx   xx                  xx       xxxxxxxxxx   xx        "+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx   xx                  xx       xx      xx   xx        "+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx   xxxxxxxxxx          xx       xx      xx   xxxxxxxxxx"+ bcolors.DEFAULT)
     print()
-    print(bcolors.FAIL+"xxxxxxxxxx   xxxxxxxxxx   xxxxxxxxxx      xxxxxxxxxx     xxxxxxxxxx"+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx      xx   xx                      xx     xx      xx"+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx      xx   xxxxxxxxxx      xxxxxxxxxx     xx      xx"+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xx      xx   xx                      xx     xx      xx"+ bcolors.ENDC)
-    print(bcolors.FAIL+"    xx       xxxxxxxxxx   xxxxxxxxxx      xxxxxxxxxx  x  xxxxxxxxxx"+ bcolors.ENDC)
+    print(bcolors.RED+"xxxxxxxxxx   xxxxxxxxxx   xxxxxxxxxx      xxxxxxxxxx     xxxxxxxxxx"+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx      xx   xx                      xx     xx      xx"+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx      xx   xxxxxxxxxx      xxxxxxxxxx     xx      xx"+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xx      xx   xx                      xx     xx      xx"+ bcolors.DEFAULT)
+    print(bcolors.RED+"    xx       xxxxxxxxxx   xxxxxxxxxx      xxxxxxxxxx  x  xxxxxxxxxx"+ bcolors.DEFAULT)
     print()
 
 def initGame():
@@ -56,17 +53,17 @@ def inputVeryfication(player, sign, board):
 
 def graphics(p):
     os.system('clear')
-    print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
-    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ p["7"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["8"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["9"]+ bcolors.ENDC,)
-    print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
-    print(bcolors.WARNING+ "==============================" + bcolors.ENDC)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
-    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ p["4"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["5"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["6"]+ bcolors.ENDC,)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
-    print(bcolors.WARNING+ "=============================="+ bcolors.ENDC)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
-    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ p["1"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["2"]+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ p["3"]+ bcolors.ENDC,)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
+    print(bcolors.YELLOW+ "        ||        ||" + bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "   "+ bcolors.DEFAULT,bcolors.BLUE+ p["7"]+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ p["8"]+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ p["9"]+ bcolors.DEFAULT,)
+    print(bcolors.YELLOW+ "        ||        ||" + bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "==============================" + bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "   "+ bcolors.DEFAULT,bcolors.BLUE+ p["4"]+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ p["5"]+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ p["6"]+ bcolors.DEFAULT,)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "=============================="+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "   "+ bcolors.DEFAULT,bcolors.BLUE+ p["1"]+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ p["2"]+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ p["3"]+ bcolors.DEFAULT,)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
 
 def check(p,P,sign):
     if ((p["1"]==sign and p["2"]==sign and p["3"]==sign)
@@ -77,32 +74,32 @@ def check(p,P,sign):
         or(p["3"]==sign and p["6"]==sign and p["9"]==sign)
         or(p["1"]==sign and p["5"]==sign and p["9"]==sign)
         or(p["3"]==sign and p["5"]==sign and p["7"]==sign)):
-        print(bcolors.FAIL+P+" " + sign + " WIN!"+bcolors.ENDC)
+        print(bcolors.RED+P+" " + sign + " WIN!"+bcolors.DEFAULT)
         return False
     return True
 
 def help():
     print("\n")
-    print(bcolors.OKGREEN+" Key input:"+ bcolors.ENDC)
+    print(bcolors.GREEN+" Key input:"+ bcolors.DEFAULT)
     print("\n")
-    print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
-    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ "7" + bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "8" + bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "9" + bcolors.ENDC,)
-    print(bcolors.WARNING+ "        ||        ||" + bcolors.ENDC)
-    print(bcolors.WARNING+ "==============================" + bcolors.ENDC)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
-    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ "4"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "5"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "6" + bcolors.ENDC,)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
-    print(bcolors.WARNING+ "=============================="+ bcolors.ENDC)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
-    print(bcolors.WARNING+ "   "+ bcolors.ENDC,bcolors.OKBLUE+ "1"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "2"+ bcolors.ENDC,bcolors.WARNING+"  ||   "+ bcolors.ENDC,bcolors.OKBLUE+ "3"+ bcolors.ENDC,)
-    print(bcolors.WARNING+ "        ||        ||"+ bcolors.ENDC)
+    print(bcolors.YELLOW+ "        ||        ||" + bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "   "+ bcolors.DEFAULT,bcolors.BLUE+ "7" + bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ "8" + bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ "9" + bcolors.DEFAULT,)
+    print(bcolors.YELLOW+ "        ||        ||" + bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "==============================" + bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "   "+ bcolors.DEFAULT,bcolors.BLUE+ "4"+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ "5"+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ "6" + bcolors.DEFAULT,)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "=============================="+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
+    print(bcolors.YELLOW+ "   "+ bcolors.DEFAULT,bcolors.BLUE+ "1"+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ "2"+ bcolors.DEFAULT,bcolors.YELLOW+"  ||   "+ bcolors.DEFAULT,bcolors.BLUE+ "3"+ bcolors.DEFAULT,)
+    print(bcolors.YELLOW+ "        ||        ||"+ bcolors.DEFAULT)
     print("\n")
 
 def checkBoard(board):
     if ' ' in board.values() :
         return True
     else:
-        print("REMIS")
+        print(bcolors.RED+"DRAW"+bcolors.DEFAULT)
         return False
 
 def game(player1,player2):
@@ -141,31 +138,41 @@ def beginGame():
   
     while True:
     #main menu
-        menu_select = input(bcolors.OKGREEN+ " Type \'s\' to start new game \n Type \'h\' for help \n Type \'q\' to quit \n:" + bcolors.ENDC)
+        menu_select = input(bcolors.GREEN+ " Type \'s\' to start new game \n Type \'h\' for help \n Type \'q\' to quit \n:" + bcolors.DEFAULT)
     #new game
         if menu_select == "s":
-            os.system('clear')
-            P1=input(bcolors.OKGREEN+ "Enter name for Player One \n :" + bcolors.ENDC)
-            P2=input(bcolors.OKGREEN+ "Enter name for Player Two \n :" + bcolors.ENDC)
-            #game
-            game(P1,P2)
-        #help
+            while True:
+                os.system('clear')
+                game_type = input(bcolors.GREEN+ "Type \'1\' for single player or \'2\' for multiplayer \n :" + bcolors.DEFAULT) 
+                if game_type == "1":
+                    P1=input(bcolors.GREEN+ "Enter name for Player One \n :" + bcolors.DEFAULT)
+                elif game_type == "2":
+                    P1=input(bcolors.GREEN+ "Enter name for Player One \n :" + bcolors.DEFAULT)
+                    P2=input(bcolors.GREEN+ "Enter name for Player Two \n :" + bcolors.DEFAULT)
+                    game(P1,P2)
+                else:
+                    print(bcolors.RED+"Invalid entry, try again."+ bcolors.DEFAULT)
+                    continue
+
         elif menu_select == "h":
-            os.system('clear')
-            help_select = input(bcolors.OKGREEN+ "Type \'r\' for rules or \'i\' for key input \n :" + bcolors.ENDC)
-            if help_select == "r":
+            while True:
                 os.system('clear')
-                print(bcolors.OKGREEN+ "Each player takes a turn and selects a field \n which they wish to place their marker on by \n typing a corresponding number. \n The goal of the game is to place 3 of your markers \n in a straight line (vertical, horizontal or diagonal)." + bcolors.ENDC)
-            elif help_select == "i":
-                os.system('clear')
-                help()
-            else:
-                print(bcolors.FAIL+"Invalid entry, try again."+ bcolors.ENDC)
-                continue
+                help_select = input(bcolors.GREEN+ "Type \'r\' for rules or \'i\' for key input \n :" + bcolors.DEFAULT)
+                if help_select == "r":
+                    os.system('clear')
+                    print(bcolors.GREEN+ "Each player takes a turn and selects a field \n which they wish to place their marker on by \n typing a corresponding number. \n The goal of the game is to place 3 of your markers \n in a straight line (vertical, horizontal or diagonal)." + bcolors.DEFAULT)
+                    break
+                elif help_select == "i":
+                    os.system('clear')
+                    help()
+                    break
+                else:
+                    print(bcolors.RED+"Invalid entry, try again."+ bcolors.DEFAULT)
+                    break
         elif menu_select == "q":
             exit(0)
         else:
-            print(bcolors.FAIL+"Invalid entry, try again."+ bcolors.ENDC)
+            print(bcolors.RED+"Invalid entry, try again."+ bcolors.DEFAULT)
         continue
 
 beginGame()
